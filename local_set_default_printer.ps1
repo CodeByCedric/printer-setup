a35c4d7062dfbe95cc8a0ff992178dd68dbd6e2e
cedric.puystjens <cedricpuystjens@outlook.com>
2024-10-22 16:23:07 +0200

    remove faulty indentation (cf. previous commit)

--- local_set_default_printer.ps1
+++ local_set_default_printer.ps1
@@ -1,65 +1,65 @@
-    [CmdletBinding()]
+[CmdletBinding()]
+param (
+    [string]$displayMacAddress
+)
+
+# Variables
+$currentUser = (whoami).Split('\\')[1]
+$printerSyncDirectory = "C:\Users\$currentUser\OneDrive - Office 365 GPI\printer\"
+$printerSyncFileName = "printerSyncConfig.txt"
+$printerLocationDownstairs = "onthaal"
+$printerLocationUpstairs = "bureau"
+$networkAdapters = Get-NetAdapter
+$macAddressFound = $false
+$regexMacAddressPattern = '^[0-9A-Fa-f]{2}([-:])[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}$'
+$validMacAddresses = (Get-NetAdapter | Select-Object -Property Name, MacAddress | ForEach-Object {"$($_.Name): $($_.MacAddress)"}) -join "`n"
+
+function Test-MacAddress {
     param (
-        [string]$displayMacAddress
+        [string]$macAddress
     )
+    return $macAddress -match $regexMacAddressPattern
+}
 
-    # Variables
-    $currentUser = (whoami).Split('\\')[1]
-    $printerSyncDirectory = "C:\Users\$currentUser\OneDrive - Office 365 GPI\printer\"
-    $printerSyncFileName = "printerSyncConfig.txt"
-    $printerLocationDownstairs = "onthaal"
-    $printerLocationUpstairs = "bureau"
-    $networkAdapters = Get-NetAdapter
-    $macAddressFound = $false
-    $regexMacAddressPattern = '^[0-9A-Fa-f]{2}([-:])[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}\1[0-9A-Fa-f]{2}$'
-    $validMacAddresses = (Get-NetAdapter | Select-Object -Property Name, MacAddress | ForEach-Object {"$($_.Name): $($_.MacAddress)"}) -join "`n"
+Write-Verbose "Starting script with parameters:"
 
-    function Test-MacAddress {
-        param (
-            [string]$macAddress
-        )
-        return $macAddress -match $regexMacAddressPattern
-    }
+# Paramater validation
+if (-not (Test-MacAddress -macAddress $displayMacAddress)) {
+    do {
+        $displayMacAddress = Read-Host "$($validMacAddresses)`nPlease provide a valid MAC-Address from the list above"
+    } while (-not (Test-MacAddress -macAddress $displayMacAddress))
+}
 
-    Write-Verbose "Starting script with parameters:"
-
-    # Paramater validation
-    if (-not (Test-MacAddress -macAddress $displayMacAddress)) {
-        do {
-            $displayMacAddress = Read-Host "$($validMacAddresses)`nPlease provide a valid MAC-Address from the list above"
-        } while (-not (Test-MacAddress -macAddress $displayMacAddress))
-    }
-
-    Write-Verbose "Display MAC-Address: $displayMacAddress"
+Write-Verbose "Display MAC-Address: $displayMacAddress"
 
 
-    # Script execution
-    try {
-        if (-not (Test-Path $printerSyncDirectory)) {
-            [void](New-Item -Path $printerSyncDirectory -ItemType Directory -ErrorAction Stop)
-            $printerSyncDirectory = Get-Item $printerSyncDirectory
-            $printerSyncDirectory.Attributes = $printerSyncDirectory.Attributes -bor 'Hidden'
-            Write-Verbose "Printer Config Directory created at $printerSyncDirectory and set to hidden"
-        } else {
-            Write-Verbose "Printer Config Directory found at $printerSyncDirectory"
-        }
-    } catch {
-        Write-Verbose "Error creating the printer config directory: $_"
-        exit
-    }
-
-    foreach ($adapter in $networkAdapters) {
-        if ($adapter.MacAddress -eq $displayMacAddress) {
-            $macAddressFound = $true
-            break
-        }
-    }
-        Write-Verbose "MAC-Address found: $macAddressFound"
-
-    if ($macAddressFound) {
-        Set-Content -Path $printerSyncDirectory\$printerSyncFileName -Value $printerLocationDownstairs
-        Write-Verbose "Printer sync file content set to '$printerLocationDownstairs'"
+# Script execution
+try {
+    if (-not (Test-Path $printerSyncDirectory)) {
+        [void](New-Item -Path $printerSyncDirectory -ItemType Directory -ErrorAction Stop)
+        $printerSyncDirectory = Get-Item $printerSyncDirectory
+        $printerSyncDirectory.Attributes = $printerSyncDirectory.Attributes -bor 'Hidden'
+        Write-Verbose "Printer Config Directory created at $printerSyncDirectory and set to hidden"
     } else {
-        Set-Content -Path $printerSyncDirectory\$printerSyncFileName -Value $printerLocationUpstairs
-        Write-Verbose "Printer sync file content set to '$printerLocationUpstairs'"
+        Write-Verbose "Printer Config Directory found at $printerSyncDirectory"
     }
+} catch {
+    Write-Verbose "Error creating the printer config directory: $_"
+    exit
+}
+
+foreach ($adapter in $networkAdapters) {
+    if ($adapter.MacAddress -eq $displayMacAddress) {
+        $macAddressFound = $true
+        break
+    }
+}
+    Write-Verbose "MAC-Address found: $macAddressFound"
+
+if ($macAddressFound) {
+    Set-Content -Path $printerSyncDirectory\$printerSyncFileName -Value $printerLocationDownstairs
+    Write-Verbose "Printer sync file content set to '$printerLocationDownstairs'"
+} else {
+    Set-Content -Path $printerSyncDirectory\$printerSyncFileName -Value $printerLocationUpstairs
+    Write-Verbose "Printer sync file content set to '$printerLocationUpstairs'"
+}
